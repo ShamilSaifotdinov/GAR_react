f6ead1c48fff367e01f6272d6205db522639fe06
ShamilSaifotdinov <shamil.saifotdinov@gmail.com>
2023-09-20 23:56:44 +0500

    Change path to gar-xml in Controler

--- GAR_table/Controllers/AdressesController.cs
+++ GAR_table/Controllers/AdressesController.cs
@@ -11,7 +11,7 @@ namespace GAR.Controllers
         #region xml
 
         // General
-        static private readonly string path = "C:\\Practice\\Web\\GAR_table\\gar-xml\\";
+        static private readonly string path = "..\\gar-xml\\";
 
         static private readonly XDocument xlevels = XDocument.Load(path + "AS_OBJECT_LEVELS_20230320_0c63ffea-e5ce-4b68-bd33-eca4ba0bb0e9.xml");
         static private readonly XElement? levels = xlevels.Element("OBJECTLEVELS");
